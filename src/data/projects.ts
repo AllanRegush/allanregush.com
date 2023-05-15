21e4c2eb43c0f495ad9f53b7f18194c1577ee7e9
Allan Regush <allan@allanregush.com>
2023-05-14 21:01:43 -0600

    Initial Commit

---
+++ src/data/projects.ts
@@ -0,0 +1,15 @@
+export type Project = {
+    title: string,
+    description: string,
+    url: string,
+    img: string
+};
+
+export const Projects = [
+    {
+        title: "Zen",
+        description: "Bare Bones OS Kernel Written in C++",
+        url: "https://github.com/AllanRegush/Zen",
+        img: "/zen.png"
+    }
+];
