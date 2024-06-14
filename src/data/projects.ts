e8c6eecef59efed54e4246b2774012e2565124f8
Allan Regush <allan@allanregush.com>
2024-06-13 19:39:57 -0600

    Add FalldaleEE

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,8 @@
 export type Project = {
     title: string,
     description: string,
-    url: string,
+    githuburl?: string,
+    liveurl?: string,
     img: string
 };
 
@@ -9,7 +10,14 @@ export const Projects: Project[] = [
     {
         title: "Zen",
         description: "Bare Bones OS Kernel Written in C++",
-        url: "https://github.com/AllanRegush/Zen",
+        githuburl: "https://github.com/AllanRegush/Zen",
         img: "/zen.png"
-    }
+    },
+    {
+        title: "Falldale EE",
+        description: "RPG Written in JavaScript - Worked in a team from Feb to May 2020",
+        githuburl: "https://github.com/gamkedo-la/falldale-ee",
+        liveurl: "https://hometeamgamedev.itch.io/falldale-ee",
+        img: "https://img.itch.zone/aW1nLzMzODI4OTkuZ2lm/original/1OvP0T.gif"
+    },
 ] as const;
